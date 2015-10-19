1bab433f8c91e4c36d2faef70aa89e372694cf47
Eugene Pempel <pempel@cleverbits.co>
2015-10-19 09:59:37 +0300

    Add CORS settings

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,13 @@ module Verbose
       # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
       # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
       # config.i18n.default_locale = :de
+
+      if (url = Rails.application.secrets.api_consumer_url).present?
+        config.action_dispatch.default_headers = {
+          "Access-Control-Allow-Origin" => url,
+          "Access-Control-Request-Method" => %w[GET POST PUT DELETE OPTIONS]
+        }
+      end
     end
   end
 end
