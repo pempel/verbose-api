eaa5e4d87d8378448d632a5f47d88d53fcb73878
Eugene Pempel <pempel@cleverbits.co>
2015-11-10 10:34:06 +0300

    Add seeds for Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,4 +5,7 @@ class Card
   field :term, type: String, default: ""
   field :definition, type: String, default: ""
   field :translation, type: String, default: ""
+
+  validates :term, presence: true
+  validates :translation, presence: true
 end
